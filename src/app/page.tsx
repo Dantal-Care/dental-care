090eb628e6519b4ae63195b2b4f1bf202e09c34c
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-07-21 15:36:40 +0530

    chore: add new components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import HeroSlider from '@/components/UI/HeroSlider/HeroSlider';
+import Navbar from '@/components/UI/Navbar/Navbar';
 import StatisticsSection from '@/components/UI/StatisticsSection';
 import TestimonialsSection from '@/components/UI/TestimonialsSection';
 
 export default function Home() {
   return (
     <div className="font-poppins">
+      <HeroSlider />
+      <Navbar />
       <StatisticsSection />
       <TestimonialsSection />
     </div>
