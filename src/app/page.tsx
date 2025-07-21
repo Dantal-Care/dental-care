06b262ccaa9ffdb4571c9dfb8d0bb72a0a386e8b
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-07-21 14:19:32 +0530

    chore: add statistics and testimonail components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import StatisticsSection from '@/components/UI/StatisticsSection';
+import TestimonialsSection from '@/components/UI/TestimonialsSection';
+
 export default function Home() {
   return (
-    <div className="font-poppins ">
-      <div className="text-White text-3xl">Dental Care</div>
-      <div className="text-primary text-xl">Primary Color</div>
-      <div className="text-secondary text-md">Secondary Color</div>
+    <div className="font-poppins">
+      <StatisticsSection />
+      <TestimonialsSection />
     </div>
   );
 }
