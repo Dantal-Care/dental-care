72656cd8bc46825cfb6970539b15869f75c2745a
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-08-01 12:03:26 +0530

    refactor: change code structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import StatisticsSection from "@/components/UI/StatisticsSection";
-import TestimonialsSection from "@/components/UI/TestimonialsSection";
-import ServiceSection from "@/components/UI/ServiceSection";
-import AboutMe from "@/components/UI/Aboutme";
-import Footer from "@/components/UI/footer/Footer";
-import HeroSlider from "@/components/UI/HeroSlider/HeroSlider";
-import Navbar from "@/components/UI/Navbar/Navbar";
+import StatisticsSection from '@/components/UI/StatisticsSection';
+import TestimonialsSection from '@/components/UI/TestimonialsSection';
+import ServiceSection from '@/components/UI/ServiceSection';
+import AboutMe from '@/components/UI/Aboutme';
+import Footer from '@/components/UI/footer/Footer';
+import HeroSlider from '@/components/UI/HeroSlider/HeroSlider';
+import Navbar from '@/components/UI/Navbar/Navbar';
 
 export default function Home() {
   return (
