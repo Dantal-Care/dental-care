bd4e34a33565a9e0e343719424076a85141d82a6
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-08-06 01:17:16 +0530

    chore: change styles

--- src/components/UI/ServiceCategory.tsx
+++ src/components/UI/ServiceCategory.tsx
@@ -15,7 +15,7 @@ export default function ServiceCategory({
     <div
       className="w-full sm:w-[300px] h-[320px] rounded-[40px] relative overflow-hidden 
              bg-cover bg-center bg-no-repeat 
-             p-6 shadow-lg flex flex-col items-center justify-end gap-3"
+             p-6 shadow-lg flex flex-col items-center gap-3"
       style={{
         backgroundImage: `
       linear-gradient(to bottom, rgba(255, 255, 255, 0.2), rgba(0, 180, 216, 1)),
@@ -23,7 +23,7 @@ export default function ServiceCategory({
     `,
       }}
     >
-      <div className="font-bold text-white text-2xl sm:text-2xl text-center">
+      <div className="font-bold text-white text-2xl sm:text-2xl text-center mt-24">
         {title}
       </div>
       <div className="text-gray-800 text-sm sm:text-base font-medium text-center">
