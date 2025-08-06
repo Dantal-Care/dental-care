9cb5d0ed8c50dc72d25ec24b752440d9b705c88d
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-08-06 14:09:08 +0530

    feat: remove button to seperate component to use component as server component

--- src/components/UI/Aboutme.tsx
+++ src/components/UI/Aboutme.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import AboutmeButton from './AboutmeButton';
 
 export default function AboutMe() {
   return (
@@ -33,22 +34,7 @@ export default function AboutMe() {
             </p>
 
             {/* About Me Button */}
-            <button className="bg-primary cursor-pointer hover:bg-primary/80 justify-center text-white px-6 py-2 rounded-full font-light transition-colors duration-200 flex text-lg items-center gap-2">
-              About me
-              <svg
-                className="w-5 h-5"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M9 5l7 7-7 7"
-                />
-              </svg>
-            </button>
+            <AboutmeButton />
           </div>
         </div>
       </div>
