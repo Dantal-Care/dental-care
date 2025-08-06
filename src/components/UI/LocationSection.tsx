f4e5355df93e2eb694272891ad01efd6107e7263
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-08-06 14:07:02 +0530

    chore: change location

--- src/components/UI/LocationSection.tsx
+++ src/components/UI/LocationSection.tsx
@@ -9,7 +9,7 @@ export default function LocationSection() {
         </h2>
         <div className="w-full md:w-[800px] h-[300px] sm:h-[400px] md:h-[450px] mt-10">
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d589.3444131166428!2d79.99321875240409!3d6.442708793769812!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2s!5e0!3m2!1sen!2slk!4v1754422979930!5m2!1sen!2slk"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3968.2913743695!2d80.53072705135739!3d5.954533495549496!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae13f3a9d89630d%3A0x781c5464ad73368e!2s74b%20Matara%20-%20Akuressa%20Hwy%2C%20Matara!5e0!3m2!1sen!2slk!4v1754465791499!5m2!1sen!2slk"
             referrerPolicy="no-referrer-when-downgrade"
             className="w-full h-full rounded-lg border-0"
             allowFullScreen
