193c1fe877076a1ba1d9af446cdc0e2de12dc7c7
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-08-15 19:03:45 +0530

    chore: remove head tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,10 +36,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </head>
       <body className={`antialiased`}>
         <main>
           {children}
